7a9e847b78ff8ccbdf164a00192b8631af25774a
Raffysul <raffysul1@gmail.com>
2023-05-18 16:21:23 +0100

    Displaying the Api data

--- lib/photo_page.dart
+++ lib/photo_page.dart
@@ -10,17 +10,56 @@ class MyPhotoPage extends StatefulWidget {
 }
 
 class _MyPhotoPageState extends State<MyPhotoPage> {
-  late Future<Photos> photoAlbum;
+  List<Photos> _photos = [];
+  final PhotosService _photoService = PhotosService();
 
   @override
   void initState() {
     super.initState();
-    photoAlbum = getPhotos();
+    _getPhotos();
   }
+  Future<void> _getPhotos() async {
+    try {
+      final photos = await _photoService.getPhotos();
+      setState(() {
+        _photos = photos;
+      });
+    } catch (e) {
+      print('Could not get photos: $e');
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
+      body: SafeArea(
+          child: _photos.isNotEmpty
+              ? GridView.builder(
+                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 2,
+                      crossAxisSpacing: 10,
+                      mainAxisSpacing: 10),
+                  itemCount: _photos.length,
+                  itemBuilder: (context, index) {
+                    final photo = _photos[index];
+                    return GridTile(
+                      footer: GridTileBar(
+                        backgroundColor: Colors.lightGreenAccent,
+                        title: Text(
+                          photo.title,
+                          textAlign: TextAlign.center,
+                        ),
+                      ),
+                      child: Image.network(
+                        photo.thumbnailUrl,
+                        fit: BoxFit.cover,
+                      ),
+                    );
+                  },
+                )
+              : const Center(
+                  child: CircularProgressIndicator(),
+                )),
     );
   }
 }
--- lib/services/photo_services.dart
+++ lib/services/photo_services.dart
@@ -3,13 +3,17 @@ import 'package:http/http.dart' as http;
 
 import '../models/photos.dart';
 
-  Future<Photos> getPhotos() async {
-   final response = await http.get(Uri.parse('https://jsonplaceholder.typicode.com/photos'));
+class PhotosService {
+  Future<List<Photos>> getPhotos() async {
+    final response = await http
+        .get(Uri.parse('https://jsonplaceholder.typicode.com/photos'));
 
     if (response.statusCode == 200) {
-      return Photos.fromJson(jsonDecode(response.body));
+      final List<dynamic> responseData = jsonDecode(response.body);
+
+      return responseData.map((json) => Photos.fromJson(json)).toList();
     } else {
       throw Exception('Failed to load Pictures');
     }
-
   }
+}
