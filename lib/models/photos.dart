306836eb446983ef12ab24b4c27cfb22091da7d6
Raffysul <raffysul1@gmail.com>
2023-05-18 13:52:01 +0100

    First commit

---
+++ lib/models/photos.dart
@@ -0,0 +1,15 @@
+class Photos {
+  final int albumId;
+  final int id;
+  final String title;
+  final String url;
+  final String thumbUrl;
+
+  Photos({
+    required this.albumId,
+    required this.id,
+    required this.title,
+    required this.url,
+    required this.thumbUrl,
+  });
+}
---
+++ lib/photo_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class MyPhotoPage extends StatefulWidget {
+  const MyPhotoPage({Key? key}) : super(key: key);
+
+  @override
+  State<MyPhotoPage> createState() => _MyPhotoPageState();
+}
+
+class _MyPhotoPageState extends State<MyPhotoPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
+  }
+}
