ff1845df5972e1b228bf5bea5fbe973c6df8ccb7
Raffysul <raffysul1@gmail.com>
2023-05-18 14:27:32 +0100

    Creating the Api class

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_interview_app/photo_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,10 +12,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyPhotos(),
+      home: const MyPhotoPage(),
     );
   }
 }
--- lib/services/photo_services.dart
+++ lib/services/photo_services.dart
@@ -1,2 +1,15 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
+
+class PhotoService {
+  Future<void> _getPhotos() async {
+    var client = http.Client;
+    var uri = Uri.parse('https://jsonplaceholder.typicode.com/photos');
+    var response = await client.get(uri);
+    if (response.statusCode == 200) {
+      var json = response.body;
+      return
+    }
+    return null;
+  }
+}
